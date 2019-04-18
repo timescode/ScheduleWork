24337af97c3f396478eff3d3cf4fc679c3145e20
zhanghang <zhanghang@LideMacBook-Air.local>
2019-04-18 16:20:22 +0800

    updat

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ScheduleWork
 This is a tool to help admin to use node.js schedule module to handle their shell and command.
+
+1.npm install node-schedule
+
+2.npm install pm2
+
+3.Test Run:
+node app.js
+
+
+4.Forever Run:
+pm2 start app.js
---
+++ app.js
@@ -0,0 +1,22 @@
+var callfile = require('child_process'); 
+var schedule = require("node-schedule");  
+var fs = require("fs");  
+
+var rule1 = new schedule.RecurrenceRule();  
+var times1    = [0,1,2,3,4,5];  
+rule1.minute  = times1;  
+schedule.scheduleJob(rule1, function(){  
+	var curTime = new Date();
+	callfile.execFile('./script.sh',[],null,function (err, stdout, stderr) {
+    	//callback(err, stdout, stderr);
+    	fs.writeFile('./doc.log', 'log:'+curTime+":"+stdout+"\r\n",{'flag':'a'},function(err){
+	   	 	if(err) console.log('Write Failed');
+	   	 	else console.log('Write Log Success');
+		});
+    	console.log(err);
+    	console.log(stdout);
+    	console.log(stderr);
+	});
+});
+
+
